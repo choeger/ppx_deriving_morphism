7f7ad57a7293a92f43bf6c9ac879f9158793ebb9
Gabriel Scherer <gabriel.scherer@gmail.com>
2019-04-23 14:12:20 +0200

    fix myocamlbuild.ml for ppx_deriving.4.3
    ppx_deriving.4.3 is going to change the directory layout of installed
    object files, so that the paths currently hardcoded in myocamlbuild.ml
    will not be valid anymore. The present commit changes myocamlbuild.ml
    to use ocamlfind packages properly.
    
    Note: "ocamlfind printppx" requires ocamlfind 1.7.0

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -5,21 +5,21 @@ let () = dispatch (
       Ocamlbuild_cppo.dispatcher hook ;
       match hook with
       | After_rules ->
-        let ppx_loc = (Findlib.query "ppx_deriving").Findlib.location in
-        let std_deriver deriver =
-          ppx_loc ^ "/" ^ deriver
-        in
-        flag ["ocaml";"link";"byte";"use_morphism"] &
-        A (ppx_loc ^ "/ppx_deriving_runtime.cma") ;
-        
-        flag ["ocaml";"link";"native";"use_morphism"] &
-        A (ppx_loc ^ "/ppx_deriving_runtime.cmxa") ;
-        
         flag ["ocaml"; "compile"; "use_morphism"] &
-        S[A"-I"; A ppx_loc ;
-          A"-ppx"; A("ocamlfind ppx_deriving/ppx_deriving "^
-                     "src/ppx_deriving_folder.cma "^
-                     "src/ppx_deriving_mapper.cma "^
-                     (std_deriver "ppx_deriving_show.cma")) ;
-         ]; 
+          Sh(String.chomp (run_and_read
+                               "ocamlfind printppx \
+                                ppx_deriving ppx_deriving.show \
+                                -ppxopt ppx_deriving,src/ppx_deriving_folder.cma \
+                                -ppxopt ppx_deriving,src/ppx_deriving_mapper.cma"));
+        let add_package tags package =
+          flag ("compile" :: "byte" :: tags)
+            (Findlib.compile_flags_byte [package]);
+          flag ("compile" :: "native" :: tags)
+            (Findlib.compile_flags_native [package]);
+          flag ("link" :: "byte" :: tags)
+            (Findlib.link_flags_byte [package]);
+          flag ("link" :: "native" :: tags)
+            (Findlib.link_flags_native [package]);
+        in
+        add_package ["ocaml"; "use_morphism"] (Findlib.query "ppx_deriving.runtime");
       | _ -> ())
